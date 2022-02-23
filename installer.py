234627291e9f435d92fb5a0f2519ceabda1d4040
antonio-vinicius-garcia-campos <antviniciuscampos@gmail.com>
2022-02-23 11:17:58 -0300

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+_______________________________________________________________________________
+~~~~~~ MODULO DE SIMULACAO ESTRUTURAL PELO METODO DOS ELEMENTOS FINITOS ~~~~~~~
+~~~~~~      Mechanical studY with Finite Element Method in PYthon       ~~~~~~~
+                              _____   _____   __  __                 
+          _ __ ___    _   _  |  ___| | ____| |  \/  |  _ __    _   _ 
+         | '_ ` _ \  | | | | | |_    |  _|   | |\/| | | '_ \  | | | |
+         | | | | | | | |_| | |  _|   | |___  | |  | | | |_) | | |_| |
+         |_| |_| |_|  \__, | |_|     |_____| |_|  |_| | .__/   \__, |
+                      |___/                           |_|      |___/ 
+
+~~~~~~                   PROGRAMA DE PROPOSITO GERAL                    ~~~~~~~  
+~~~~~~                  copyright all rights reserved                   ~~~~~~~
+===============================================================================
+@author: ANTONIO VINICIUS GARCIA CAMPOS
+@copyright: 3D EASYCAE SERVIÇOS DE ANÁLISE COMPUTACIONAL
+@licence: GPL-3.0 License
+    
+Copyright (c) 2021, 3D EASYCAE SERVIÇOS DE ANÁLISE COMPUTACIONAL
+All rights reserved.
+"""
+
+#%% INSTALL & SETUP SYSTEM
