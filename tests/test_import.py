49c852a08bd28d5b059f5c7960070ce2d7fbe985
Vinicius Campos <antviniciuscampos@gmail.com>
2023-02-11 18:03:41 -0300

    Create test_import.py

---
+++ tests/test_import.py
@@ -0,0 +1,9 @@
+'''
+TEST TO IMPORT MODULES FROM MYFEMPY
+'''
+
+from myfempy.mesh.genmesh import ModelGen
+from myfempy.core.solver import Solver
+from myfempy.postprc.postcomp import PostProcess
+from myfempy.plots.postplot import postproc_plot
+from myfempy.plots.prevplot import preview_plot
